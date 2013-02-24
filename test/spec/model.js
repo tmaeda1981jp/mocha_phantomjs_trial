33acef22d6bfaa104f51e237ad1d3b2e1d4b79de
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-02-25 04:43:19 +0900

    Add tests for model

--- test/spec/model.js
+++ test/spec/model.js
@@ -1,13 +1,43 @@
 /*jslint white: true, nomen: true, maxlen: 120, plusplus: true, */
 /*global _:false, $:false, define:false, require:false, describe:false,
  it:false, before:false, after:false, beforeEach:false, afterEach:false, 
- sinon:false, */
+ sinon:false, context:false, */
 'use strict';
 
-define([], function() {
-  describe('1+1', function() {
-    it('1+1=2', function() {
-      (1+1).should.be.equal(2);
+define(['../../app/model'], function(Counter) {
+  describe('#increment', function() {
+    it('countが1増えること．', function() {
+      var counter = new Counter(),
+          current = counter.get('count');
+      counter.increment();
+      counter.get('count').should.equal(current + 1);
+    });
+  });
+
+  describe('#decrement', function() {
+    context('countが0の場合', function() {
+      it('0のままであること', function() {
+        var counter = new Counter();
+        counter.decrement();
+        counter.get('count').should.equal(0);
+      });
+    });
+    context('countが1以上の場合', function() {
+      it('1減ること', function() {
+        var counter = new Counter();
+        counter.set('count', 5);
+        counter.decrement();
+        counter.get('count').should.equal(4);
+      });
+    });
+  });
+
+  describe('#reset', function() {
+    it('countが0になること', function() {
+      var counter = new Counter();
+      counter.set('count', 5);
+      counter.reset();
+      counter.get('count').should.equal(0);
     });
   });
 });
